80907afe8cab4c752674eea2b3249cfbd4d3f530
lauer3912 <lauer3912@gmail.com>
2024-04-06 07:22:48 +0800

    init

--- init.js
+++ init.js
@@ -8,9 +8,9 @@ const fs = require('fs');
 const path = require('path');
 
 // ------------------------------------------------------------------------------------------- 以下内容需要自己替换
-const subSiteAuthor = 'Nova';
-const subSiteKeywords = `` + ','; // 关键字，多个关键字用逗号分隔
-const indexNowKey = 'ec9c9516ea82433086d979e16e1960e0'; // 替换为你的API密钥
+const subSiteAuthor = 'Peter';
+const subSiteKeywords = `android unlock, android screen unlock, android password unlock, unlock android, unlock android phone` + ','; // 关键字，多个关键字用逗号分隔
+const indexNowKey = 'dff9dc1d92564e29bee1b4bcc5daf47c'; // 替换为你的API密钥
 
 
 
